7ab11387b298eadb8ee611dab750987a6cc1ddc1
Mykty <baha@Bahas-MacBook-Pro.local>
2022-08-09 19:34:51 +0600

    task4

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int res = (9*a*a - 5*b + 2 + a - 7) * ((a+b - 4 * a * b)/ 2);
+        System.out.println(res);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+
+        char a = 97;
+        char c = 99;
+        char h = 104;
+        char r = 114;
+
+        System.out.printf("%s %s %s %s ",c,h,a,r);
     }
 }
